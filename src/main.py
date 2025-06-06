4fdafc103eccdf5caed2abfb00d3c77c237b0202
Leo <leonsiypopov@gmail.com>
2025-06-06 22:55:10 +0300

    done2

--- src/main.py
+++ src/main.py
@@ -19,8 +19,8 @@ app = FastAPI(
     lifespan=lifespan
 )
 
-app.include_router(movies.router, prefix="/api/v1/theater/movies", tags=["movies"])
-
 api_version_prefix = "/api/v1"
+app.include_router(movies.router, prefix=api_version_prefix, tags=["movies"])
+
 
 app.include_router(movie_router, prefix=f"{api_version_prefix}/theater", tags=["theater"])
